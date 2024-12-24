e174d54359b7e70d1a027b76782e92af75d67735
ZineddineBE <beouche.zineddine@gmail.com>
2024-12-24 12:08:56 +0100

    Supression du fichier mail.php

--- mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$email = $_POST['email'];
-$to = $email;
-$subject = "Portfolio : Message de s$_POST['name'] $_POST['lastName'];";
-$txt = $_POST['message'];
-$headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
-
-mail($to,$subject,$txt,$headers);
-?>
