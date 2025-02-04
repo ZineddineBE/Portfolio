1d4c50fa09db13ceb63bfd4d6b0a4fb9fe3e835d
ZineddineBE <beouche.zineddine@gmail.com>
2025-02-04 09:33:10 +0100

    Amélioration de la responsivité + du burger menu et de son comportement

--- script.js
+++ script.js
@@ -45,21 +45,41 @@ presentation.addEventListener("click", function(f) {
 });
 
 // Burger menu
-const menuHamburger = document.querySelector(".burger-menu")
-const navLinks = document.querySelector(".nav-links")
- 
-menuHamburger.addEventListener('click',()=>{
-navLinks.classList.toggle('mobile-menu')
-})
+const menuHamburger = document.getElementById("burger-menu");
+const navLinks = document.querySelector(".nav-links");
+
+menuHamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('mobile-menu');
+    menuHamburger.classList.add('fade'); // Ajoute l'animation
+
+    setTimeout(() => {
+        if (navLinks.classList.contains('mobile-menu')) {
+            menuHamburger.src = "img/cross.png";
+
+            menuHamburger.style.zIndex = '999';
+        } else {
+            menuHamburger.src = "img/menu-btn-dark.png";
+            menuHamburger.style.width = '35px';
+            menuHamburger.style.zIndex = '1';
+        }
+        menuHamburger.classList.remove('fade'); // Supprime l'animation après la transition
+    }, 150); // Durée de l'effet (doit correspondre à la durée CSS)
+});
 
 var links = document.querySelectorAll('a');
 
-for( var i=links.length; i--; ) {
-    links[i].addEventListener("click", function(){
-    navLinks.classList.remove("mobile-menu");    
+for (var i = links.length; i--;) {
+    links[i].addEventListener("click", function () {
+        navLinks.classList.remove("mobile-menu");
+        menuHamburger.classList.add('fade');
+
+        setTimeout(() => {
+            menuHamburger.src = "img/menu-btn-dark.png";
+            menuHamburger.style.zIndex = '1';
+            menuHamburger.classList.remove('fade');
+        }, 150);
     });
 };
-
 
 // Add class "active" of nav-item when one is selected
 const navMenu = document.getElementById('nav-links');
@@ -76,3 +96,10 @@ for(var i = 0; i < navItems.length; i++){
     })
 }
 
+const navBar = document.querySelector("mobile-menu");
+
+// Récupérer le style calculé
+const navBarStyle = window.getComputedStyle(navBar);
+
+console.log("Hauteur CSS de la navbar :", navBarStyle.height);
+console.log("Largeur CSS de la navbar :", navBarStyle.width);
